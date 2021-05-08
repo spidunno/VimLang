d62cbf31a3e8d247018f1af0703f2c887dc31c59
spidunno <70819261+spidunno@users.noreply.github.com>
2021-05-08 15:40:11 -0400

    Create index.ts

---
+++ index.ts
@@ -0,0 +1,14 @@
+class VimLang {
+	static parse(source, strictParsing = true) {
+		/* test */
+		if(!strictParsing) {console.log("bitch you thought;");return false;};
+
+	}
+}
+
+
+function main() {
+	VimLang.parse();
+};
+
+main();
